af3bc7c90fcf6a029f55ac0b3ec04b05cc67f117
Alessio Bianchi <venator85@gmail.com>
2016-07-03 22:31:11 +0200

    log to file

--- log/src/main/java/eu/alessiobianchi/log/Log.java
+++ log/src/main/java/eu/alessiobianchi/log/Log.java
@@ -1,20 +1,66 @@
 package eu.alessiobianchi.log;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
-public class Log {
-
+public final class Log {
 	private static final int MAXIMUM_LINE_LENGTH = 4000;
+	private static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("HH:mm:ss.SSS", Locale.US);
+	private static final String MSG_FORMAT = "%s [%s][%s]:   %s\n";
+	private static final Object lock = new Object();
 
+	private static File logFile;
+	private static BufferedWriter writer;
 	private static boolean enabled = true;
+
+	public static boolean isEnabled() {
+		return enabled;
+	}
 
 	public static void setEnabled(boolean enabled) {
 		Log.enabled = enabled;
 	}
 
-	public static boolean isEnabled() {
-		return enabled;
+	public static void init(boolean enableLogcat, File logFile) {
+		enabled = enableLogcat;
+
+		Log.logFile = logFile;
+		if (logFile != null) {
+			synchronized (lock) {
+				if (writer != null) {
+					try {
+						writer.close();
+					} catch (IOException ignore) {
+					}
+				}
+				try {
+					writer = new BufferedWriter(new FileWriter(logFile, false));
+				} catch (IOException e) {
+					throw new RuntimeException(e.getMessage(), e);
+				}
+			}
+		}
+	}
+
+	public static File getLogFile() {
+		synchronized (lock) {
+			if (writer != null) {
+				try {
+					writer.flush();
+				} catch (IOException e) {
+					logcat(android.util.Log.ERROR, "Log", "Error flushing log file");
+					logcat(android.util.Log.ERROR, "Log", getStackTraceString(e));
+				}
+			}
+			return logFile;
+		}
 	}
 
 	public static void v(String msg, Throwable t, Object tag) {
@@ -58,9 +104,24 @@ public class Log {
 	}
 
 	private static void log(int level, String tag, String msg, Throwable t) {
-		if (!enabled) {
-			return;
+		final String stacktrace = t != null ? getStackTraceString(t) : null;
+
+		logcat(level, tag, msg);
+		if (stacktrace != null) {
+			logcat(level, tag, stacktrace);
 		}
+
+		if (writer != null) {
+			synchronized (lock) {
+				logToFile(level, tag, msg);
+				if (stacktrace != null) {
+					logToFile(level, tag, stacktrace);
+				}
+			}
+		}
+	}
+
+	private static void logcat(int level, String tag, String msg) {
 		final int msgLen = msg.length();
 		if (msgLen > MAXIMUM_LINE_LENGTH) {
 			for (int i = 0; i < msgLen; i++) {
@@ -69,20 +130,36 @@ public class Log {
 				do {
 					int end = Math.min(newline, i + MAXIMUM_LINE_LENGTH);
 					String part = msg.substring(i, end);
-					invokeLogger(level, tag, part);
+					android.util.Log.println(level, tag, part);
 					i = end;
 				} while (i < newline);
 			}
 		} else {
-			invokeLogger(level, tag, msg);
-		}
-		if (t != null) {
-			log(level, tag, getStackTraceString(t), null);
+			android.util.Log.println(level, tag, msg);
 		}
 	}
 
-	private static void invokeLogger(int level, String tag, String msg) {
-		android.util.Log.println(level, tag, msg);
+	private static void logToFile(int level, String tag, String msg) {
+		try {
+			String sLevel;
+			if (level == android.util.Log.VERBOSE) {
+				sLevel = "V";
+			} else if (level == android.util.Log.DEBUG) {
+				sLevel = "D";
+			} else if (level == android.util.Log.INFO) {
+				sLevel = "I";
+			} else if (level == android.util.Log.WARN) {
+				sLevel = "W";
+			} else if (level == android.util.Log.ERROR) {
+				sLevel = "E";
+			} else {
+				sLevel = String.valueOf(level);
+			}
+			writer.write(String.format(Locale.US, MSG_FORMAT, TIMESTAMP_FORMAT.format(new Date()), sLevel, tag, msg));
+		} catch (IOException e) {
+			logcat(android.util.Log.ERROR, "Log", "Error writing log to file");
+			logcat(android.util.Log.ERROR, "Log", getStackTraceString(e));
+		}
 	}
 
 	private static String getStackTraceString(Throwable tr) {
