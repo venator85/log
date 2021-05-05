353f150945546aed4411f80770f883a4e54be9a0
Alessio Bianchi <venator85@gmail.com>
2021-05-05 13:13:05 +0200

    release script

--- release.sh
+++ release.sh
@@ -7,3 +7,7 @@ mkdir -p log-noop/api/
 cp log/api/log.api log-noop/api/log-noop.api
 
 ./gradlew :log-noop:apiCheck
+
+./gradlew publish
+
+/Users/venator/git/venator85-maven/generator/maven-generator-1.0-SNAPSHOT-all
